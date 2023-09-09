fcfd403534baf81d157619eb24a79b7775070ce5
Andrei Faur <da3drus@gmail.com>
2023-09-09 22:06:48 +0200

    Rework setup to include power supply and 2nd RPI
    - Add udev rules for the power supply and the two debug probes
    - Upgrade Rust version
    - Remove udev-related commands from agent Dockerfile
    - Install Python packages using pip3
    - Add new env variables to compose file to help with connecting to
      the power supply and the probes
    - Add symlink to power supply tty in agent Dockerfile (since cargo flash
      uses VID:PID:Serial then we don't have to do that for the debug
      probes)

--- agent_startup.sh
+++ agent_startup.sh
@@ -3,6 +3,9 @@
 # Script adapted from
 # https://docs.docker.com/config/containers/multi-service_container/
 
+# Add symlink to power supply /dev entry
+ln -s /dev/${DEV_POWER_SUPPLY} /dev/ttyPowerSupply
+
 # Start the Logic2 Automation API
 xvfb-run /opt/Logic2 --automation --no-sandbox --disable-gpu --headless > /tmp/saleae.log 2>&1 &
 
